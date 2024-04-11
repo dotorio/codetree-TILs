35125b8589191f0f9c1ce5ffd164e2f2cd2c6be1
dotorio <139529523+dotorio@users.noreply.github.com>
2024-04-11 21:58:28 +0900

    Title: 개발자의 공놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240411/개발자의 공놀이/developer's-ball-game.py
+++ 240411/개발자의 공놀이/developer's-ball-game.py
@@ -1,6 +1,7 @@
 N = int(input())
 devs = list(map(int, input().split()))
 devs.sort()
+
 ball, dir, idx = 0, 0, 0
 while True:
     start = devs[idx]
@@ -10,6 +11,7 @@ while True:
         break    
     next = devs[idx]
     dist = next - start
+    
     idx += 1
     while idx != N:
         if not dir:
@@ -18,20 +20,25 @@ while True:
             else:
                 dir = 2
             dist = devs[idx] - next
+            
             next = devs[idx]
             idx += 1
         elif dir == 1:
             if dist > devs[idx] - next:
                 break
             dist = devs[idx] - next
+            
             next = devs[idx]
             idx += 1
         else:
             if dist <= devs[idx] - next:
-                idx += 1
+                
                 break
             dist = devs[idx] - next
+            
             next = devs[idx]
             idx += 1
     ball += 1
+    dir = 0
+
 print(ball)
