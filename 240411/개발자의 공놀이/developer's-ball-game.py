11d270351a9df06de32039160652d6c221645881
dotorio <139529523+dotorio@users.noreply.github.com>
2024-04-11 17:30:06 +0900

    Title: 개발자의 공놀이, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240411/개발자의 공놀이/developer's-ball-game.py
+++ 240411/개발자의 공놀이/developer's-ball-game.py
@@ -1,18 +1,36 @@
 N = int(input())
 devs = list(map(int, input().split()))
 devs.sort()
-ball = 0
+ball, btn = 0, 0
 while devs:
     start = devs.pop()
     if not devs:
         ball += 1
         break
     next = devs.pop()
-    min_dis = start - next
+    dis = start - next
     while devs:
-        if min_dis < next - devs[-1]:
-            break
-        min_dis = next - devs[-1]
-        next = devs.pop()
+        if not btn:
+            next2 = devs[-1]
+            if dis < next - next2:
+                # 오름차순
+                btn = 1            
+            else:
+                # 내림차순
+                btn = 2
+            dis = next - next2
+            next = devs.pop()
+        elif btn == 1:
+            next2 = devs[-1]
+            if dis >= next - next2:
+                break
+            dis = next - next2
+            next = devs.pop()
+        else:
+            next2 = devs[-1]
+            if dis < next - next2:
+                break
+            dis = next - next2
+            next = devs.pop()            
     ball += 1
 print(ball)
